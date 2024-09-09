3e2f241075e1510d8451b5fc53e7d41b0fcd1ea1
olusegun <olusegunbamgbelu755@gmail.com>
2024-09-09 04:21:34 +0100

    debug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,css,js,images}"],
+  content: [
+    "./public/**/*.{html,css,js}"
+  ],
   theme: {
     screens: {
       'sm': {'max': '870px'},
